1ebf4211f1663bcd6414c3cf06c58ff8d7e530ba
Linus <laei@zhaw.ch>
2024-03-17 21:50:45 +0100

    added possibility to overwrite sticker keywords

--- functions/sticker_functions.py
+++ functions/sticker_functions.py
@@ -38,24 +38,43 @@ async def sticker(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     file_unique_id = update.message.sticker.file_unique_id
     emojies = update.message.sticker.emoji
     pprint(update.message.sticker)
-    context.user_data["sticker"] = (user_id, file_id, file_unique_id, emojies)
-    await update.message.reply_text(
-        "What keywords do you want to attach to this sticker?"
+    # Check if sticker is already in DB
+    c.execute(
+        "SELECT keywords FROM stickers WHERE user_id = ? AND file_unique_id = ?",
+        (user_id, file_unique_id),
     )
+    results = c.fetchall()
+    if len(results) > 0:
+        current_keywords = results[0][0]
+        await update.message.reply_text(f"This sticker is already saved!\nCurrent keywords: {current_keywords}")
+        await update.message.reply_text("If you want to change the keywords, send new ones.\nOr send /cancel to cancel.")
+        exists = True
+    else:
+        await update.message.reply_text(
+            "What keywords do you want to attach to this sticker?"
+        )
+        exists = False
+    context.user_data["sticker"] = (user_id, file_id, file_unique_id, emojies, exists)
     return KEYWORDS
 
 #### Step 2: Save the sticker
 async def keywords(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     keywords = update.message.text
-    user_id, file_id, file_unique_id, emojies = context.user_data["sticker"]
+    user_id, file_id, file_unique_id, emojies, exists = context.user_data["sticker"]
     # pack_id = "default"
     pack_id = await get_current_pack(user_id)
     print(user_id, pack_id, file_unique_id, file_id, keywords, emojies)
     try:
-        c.execute(
-            "INSERT INTO stickers (user_id, pack_id, file_unique_id, file_id, keywords, emojies) VALUES (?, ?, ?, ?, ?, ?)",
-            (user_id, pack_id, file_unique_id, file_id, keywords, emojies),
-        )
+        if exists:
+            c.execute(
+                "UPDATE stickers SET keywords = ? WHERE user_id = ? AND file_unique_id = ? AND pack_id = ?",
+                (keywords, user_id, file_unique_id, pack_id),
+            )
+        else:
+            c.execute(
+                "INSERT INTO stickers (user_id, pack_id, file_unique_id, file_id, keywords, emojies) VALUES (?, ?, ?, ?, ?, ?)",
+                (user_id, pack_id, file_unique_id, file_id, keywords, emojies),
+            )
         conn.commit()
         await update.message.reply_text("Sticker saved!")
     except Exception as e:
@@ -78,10 +97,11 @@ async def delete_sticker(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
 async def deletesticker(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     user_id = update.message.from_user.id
     file_unique_id = update.message.sticker.file_unique_id
+    pack_id = await get_current_pack(user_id)
     pprint(update.message.sticker)
     c.execute(
-        "DELETE FROM stickers WHERE user_id = ? AND file_unique_id = ?",
-        (user_id, file_unique_id),
+        "DELETE FROM stickers WHERE user_id = ? AND file_unique_id = ? AND pack_id = ?",
+        (user_id, file_unique_id, pack_id),
     )
     conn.commit()
     await update.message.reply_text("Sticker deleted!")
